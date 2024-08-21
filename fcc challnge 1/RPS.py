76ce692af28c21dbd272782456ae974c5bf506f3
khiro45 <khiro.thori@gmail.com>
2024-08-21 11:36:08 +0100

    Initial commit

---
+++ fcc challnge 1/RPS.py
@@ -0,0 +1,20 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
+from RPS import player ,player2, player3
+from unittest import main
+
+play(player, quincy, 1000)
+play(player, abbey, 1000)
+play(player2, kris, 1000)
+play(player3, mrugesh, 1000)
+
+# Uncomment line below to play interactively against a bot:
+# play(human, abbey, 20, verbose=True)
+
+# Uncomment line below to play against a bot that plays randomly:
+# play(human, random_player, 1000)
+
+
+
+# Uncomment line below to run unit tests automatically
+# main(module='test_module', exit=False)
